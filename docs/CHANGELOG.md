d87ed993bf0704d64dabd1e681d36f477f04282d
schizoprada <150098802+schizoprada@users.noreply.github.com>
2026-02-06 10:18:19 -0500

    0.0.0

---
+++ docs/CHANGELOG.md
@@ -0,0 +1,4 @@
+# `typical` -- changelog
+
+## [0.0.0] -- Feb. 06, 2026
+* Project Initialized
---
+++ src/typical/__init__.py
@@ -0,0 +1,7 @@
+"""
+typical
+"""
+__author__ = "Joel Yisrael"
+__version__ = "0.0.0"
+
+VERSION = tuple(map(int, __version__.split('.')))
